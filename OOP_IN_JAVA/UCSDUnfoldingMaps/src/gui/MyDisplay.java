2d1f9f6baf9d731c33cbca37ec4e20c870d6f945
Peter <pitrens.studio@gmail.com>
2019-03-05 16:30:52 +0800

    keep gooing deeper through processing doc

---
+++ OOP_IN_JAVA/UCSDUnfoldingMaps/src/gui/MyDisplay.java
@@ -0,0 +1,36 @@
+package gui;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class MyDisplay extends PApplet {
+  static public void main(String args[]) {
+    PApplet.main(new String[]{"gui.MyDisplay"});
+  }
+
+  PImage img;
+
+
+  public void setup() {
+    size(400, 400);
+    background(255);
+
+  }
+
+  public void draw() {
+
+//    fill(211,22,32);
+//    ellipse(100,150,75,75);
+//    ellipse(300,150,75,75);
+//
+//
+//    fill(0,0,0);
+//    ellipse(100,150,30,30);
+//    ellipse(300,150,30,30);
+//
+//    noFill();
+//    arc(200,280, 100,100 , 0, PI);
+
+
+  }
+}
--- OOP_IN_JAVA/UCSDUnfoldingMaps/src/playground/PAppletTest.java
+++ OOP_IN_JAVA/UCSDUnfoldingMaps/src/playground/PAppletTest.java
@@ -16,23 +16,43 @@ public class PAppletTest extends PApplet {
 
   public void setup() {
     String imageUrl = "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b";
+    background(255);
+
     webImage = loadImage(imageUrl, "jpeg");
     webImage.resize(0 , SCREEN_HEIGHT);
+
+    // 0,0 means I need to have the top left corner of the image at x=0 , y=0
+    image(webImage , 0, 0); // display image
 
     size(SCREEN_WIDTH, SCREEN_HEIGHT);
 
   }
 
   public void draw() {
-    System.out.println("running");
+    int [] color = sunColorSec(second()); //calculate color code
 
-    background(0);
+    fill(color[0], color[1], color[2]);
 
-    // 0,0 means I need to have the top left corner of the image at x=0 , y=0
-    image(webImage , 0, 0);
-
-    fill(255,209,0);
     // width and height are dynamic value about the canvas size
     ellipse(width/4, height/5, width/4 , height/4);
+  }
+
+  public int[] sunColorSec(float seconds)
+  {
+    int[] rgb = new int[3];
+
+    // scale the brigthness of the yellow based on the current seconds. 30 sec is black.
+    // 0 sec is bright yellow.
+    float diffFrom30 = Math.abs(30-seconds);
+
+
+    float ratio = diffFrom30 / 30;
+    rgb[0] = (int) (255 * ratio);
+    rgb[1] = (int) (255 * ratio);
+    rgb[2] = 0;
+
+    return rgb;
+
+
   }
 }
